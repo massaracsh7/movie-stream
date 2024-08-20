d22d0388f24b082011f6cdbc90459c100e3b7f10
massaracsh7 <fatum7@gmail.com>
2024-08-20 13:44:18 +0300

    feat: add images and types

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+export const TOAST_NO_CONNECTION = 'No internet connection. Please check your network.';
+export const TOAST_SIGN_IN_PENDING = 'Signing in...';
+export const TOAST_SIGN_UP_PENDING = 'Signing up...';
+export const TOAST_UPDATE_PENDING = 'Updating ...';
+export const TOAST_SIGN_IN_SUCCESS = 'Sign in successful!';
+export const TOAST_SIGN_UP_SUCCESS = 'Sign up successful!';
+export const TOAST_UPDATE_SUCCESS = 'Update successful!';
+export const TOAST_SIGN_IN_ERROR = 'Incorrect login or password. Please try again!';
+export const TOAST_INTERNAL_SERVER_ERROR = 'Internal server error. Please try again!';
+export const TOAST_SIGN_UP_ERROR =
+  'This email already exists. Please log in or use another email address!';
+export const ASCENDING_SORT_ORDER = 'asc';
+export const DESCENDING_SORT_ORDER = 'desc';
+export const FROM_ZERO_TO_TEN_PRICE_RANGE = ['*', '999'];
+export const FROM_TEN_TO_TWENTY_PRICE_RANGE = ['1000', '1999'];
+export const FROM_TWENTY_AND_MORE = ['2000', '*'];
+export const SORTING_PARAM_NAME = 'name';
+export const SORTING_PARAM_PRICE = 'price';
+export const TOAST_UPDATE_ERROR = 'Update error. Check your fields';
+export const TOAST_PASSWORD_ERROR = 'The given current password does not match';
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );

a62d90873eeb802b8531c407425a1027f9b7b71f
massaracsh7 <fatum7@gmail.com>
2024-08-20 14:36:21 +0300

    feat: add basic styles, fonts, favicon

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <p>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
